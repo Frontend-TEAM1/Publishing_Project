de71d6e033ab7247c4a2670e65db4f824dde4dc9
YoungSeungJang <1345620@naver.com>
2022-12-22 17:13:22 +0900

    Docs,Feat
    header,footer html,css,js 추가, whoWeAre헤더푸터추가

---
+++ js/headerFooter.js
@@ -0,0 +1,18 @@
+const $header = document.querySelector('header');
+
+
+document.addEventListener('mousewheel', (e)=>{
+    console.log(e);
+    console.log(document.documentElement);
+    if(e.wheelDelta>0){
+        $header.style.position = 'sticky';
+        $header.style.top = '0';
+        $header.style.zIndex = '999';
+        $header.style.backgroundColor = '#fff'
+    }
+    if(e.wheelDelta<0){
+        $header.style.position = '';
+        $header.style.top = '';
+        $header.style.zIndex = '';
+    }
+})
